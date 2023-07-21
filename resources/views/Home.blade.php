b4ad5d57d489c558d9f5e38002d04ad25b71cb45
S-Lombardi <silo.lombardi@gmail.com>
2023-07-21 15:17:07 +0200

    Creata struttura base dell'header e nella cartella partials il file header.blade

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -16,8 +16,58 @@
 </head>
 
 <body>
-
    
+   <header >
+        <div class="container">
+            <div class="row a ">
+                <!-- COL LEFT -->
+                <div class="col">
+                    <!-- Logo -->
+                    <div>
+                        <img src="{{Vite::asset('resources/img/dc-logo.png')}}" >
+                    </div>
+                </div>
+                <!-- FINE COL LEFT -->
+
+                <!-- COL RIGHT -->
+                <div class="col">
+                    <ul class= "d-flex align-items-center h-100">
+                        <li>
+                            <a href="">Characters</a>
+                        </li>
+                        <li>
+                            <a href="">Comics</a>
+                        </li>
+                        <li>
+                            <a href="">Movies</a>
+                        </li>
+                        <li>
+                            <a href="">Games</a>
+                        </li>
+                        <li>
+                            <a href="">Collectibles</a>
+                        </li>
+                        <li>
+                            <a href="">Videos</a>
+                        </li>
+                        <li>
+                            <a href="">Fans</a>
+                        </li>
+                        <li>
+                            <a href="">News</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- FINE COL RIGHT -->
+
+
+
+            </div>
+        </div>
+
+
+
+    </header>
 
 </body>
 
