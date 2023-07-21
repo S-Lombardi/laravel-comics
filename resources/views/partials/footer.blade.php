1d6ca819a13857148d5e5d213a5edbd3c7f20c0e
S-Lombardi <silo.lombardi@gmail.com>
2023-07-21 19:05:26 +0200

    Aggiunto footer middle

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -41,10 +41,72 @@
                 </ul>
                 <!-- FINE Footer top - BLU -->
             </div>
+
+            <!-- Footer middle- Link -->
+            <div class="d-flex flex-column" style="background-image:url({{Vite::asset('/resources/img/footer-bg.jpg')}})">
+                <div class="col-12 d-flex justify-content-between " >
+                        
+                    <div class=" d-flex mt-5 ms-5">
+                        <div>
+                            <h5 class="fw-bold text-white ">DC COMICS</h5>
+                            <ul class=" p-0">
+                                <a href="/" class="text-decoration-none text-secondary"><li>Characters</li></a>
+                                <a href="/" class="text-decoration-none text-secondary"><li>Comics</li></a>
+                                <a href="/" class="text-decoration-none text-secondary"><li>Movies</li></a>
+                                <a href="/" class="text-decoration-none text-secondary"><li>TV</li></a>
+                                <a href="/" class="text-decoration-none text-secondary"><li>Games</li></a>
+                                <a href="/" class="text-decoration-none text-secondary"><li>Videos</li></a>
+                                <a href="/" class="text-decoration-none text-secondary"><li>News</li></a>
+                            </ul>
+        
+                            <h5 class="fw-bold text-white ">SHOP</h5>
+                            <ul class="p-0">
+                                <a href="/" class="text-decoration-none text-secondary"><li>Shop DC</li></a>
+                                <a href="/" class="text-decoration-none text-secondary"><li>Shop DC Collectibles</li></a>
+                            </ul>
+                        </div>
+
+                        <div class="ms-5">
+                            <h5 class="fw-bold text-white">DC</h5>
+                                <ul class="p-0">
+                                    <a href="/" class="text-decoration-none text-secondary"><li>Terms Of Use</li></a>
+                                    <a href="/" class="text-decoration-none text-secondary"><li>Privacy policy (New)</li></a>
+                                    <a href="/" class="text-decoration-none text-secondary"><li>Ad Choice</li></a>
+                                    <a href="/" class="text-decoration-none text-secondary"><li>Advertising</li></a>
+                                    <a href="/" class="text-decoration-none text-secondary"><li>Jobs</li></a>
+                                    <a href="/" class="text-decoration-none text-secondary"><li>Subscriptions</li></a>
+                                    <a href="/" class="text-decoration-none text-secondary"><li>Talent Workshops</li></a>
+                                    <a href="/" class="text-decoration-none text-secondary"><li>CPSC Certificates</li></a>
+                                    <a href="/" class="text-decoration-none text-secondary"><li>Ratings</li></a>
+                                    <a href="/" class="text-decoration-none text-secondary"><li>Shop Help</li></a>
+                                    <a href="/" class="text-decoration-none text-secondary"><li>Contact Us</li></a>
+                                </ul>
+                        </div>
+
+                        <div class="ms-4">
+                            <h5 class="fw-bold text-white">SITES</h5>
+                                <ul class="p-0" >
+                                    <a href="/" class="text-decoration-none text-secondary"><li>DC</li></a>
+                                    <a href="/" class="text-decoration-none text-secondary"><li>MAD Magazine</li></a>
+                                    <a href="/" class="text-decoration-none text-secondary"><li>DC Kids</li></a>
+                                    <a href="/" class="text-decoration-none text-secondary"><li>DC Universe</li></a>
+                                    <a href="/" class="text-decoration-none text-secondary"><li>DC Power Visa</li></a>
+                                </ul>
+                        </div>
+                    </div>    
+
+                    <div>
+                        <img class="w-75" src="{{Vite::asset('./resources/img/dc-logo-bg.png')}}" alt="">
+                    </div>
+                </div>
+                <div class= "ms-5">
+                    <span class="text-secondary">
+                        All Site Content TM and © 2020 DC Entertainment, unless otherwise <a href="/" class="text-decoration-none">noted here</a>. All rights reserved. <br> <a href="/" class="text-decoration-none">Cookies Settings</a>
+                    </span>
+                </div>
+            </div>
+            <!-- FINE Footer middle - Link -->
         </div>
     </div>
 
-    <div class="container">
-        
-    </div>
 </footer>
