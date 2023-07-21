0d9bae0e20dd7e3969b153f585f7eb6164f5036a
S-Lombardi <silo.lombardi@gmail.com>
2023-07-21 15:22:50 +0200

    Spostato l'header nella repo partials e incluso nel file home.blade

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -17,56 +17,8 @@
 
 <body>
    
-   <header >
-        <div class="container">
-            <div class="row a ">
-                <!-- COL LEFT -->
-                <div class="col">
-                    <!-- Logo -->
-                    <div>
-                        <img src="{{Vite::asset('resources/img/dc-logo.png')}}" >
-                    </div>
-                </div>
-                <!-- FINE COL LEFT -->
-
-                <!-- COL RIGHT -->
-                <div class="col">
-                    <ul class= "d-flex align-items-center h-100">
-                        <li>
-                            <a href="">Characters</a>
-                        </li>
-                        <li>
-                            <a href="">Comics</a>
-                        </li>
-                        <li>
-                            <a href="">Movies</a>
-                        </li>
-                        <li>
-                            <a href="">Games</a>
-                        </li>
-                        <li>
-                            <a href="">Collectibles</a>
-                        </li>
-                        <li>
-                            <a href="">Videos</a>
-                        </li>
-                        <li>
-                            <a href="">Fans</a>
-                        </li>
-                        <li>
-                            <a href="">News</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- FINE COL RIGHT -->
-
-
-
-            </div>
-        </div>
-
-
-
+    <header>
+        @include('partials.header')
     </header>
 
 </body>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,47 @@
+<!-- HEADER -->
+<div class="container">
+    <div class="row">
+        
+        <!-- COL LEFT -->
+        <div class="col">
+            <!-- Logo -->
+            <div>
+                <img src="{{Vite::asset('resources/img/dc-logo.png')}}" >
+            </div>
+        </div>
+        <!-- FINE COL LEFT -->
+
+        <!-- COL RIGHT -->
+        <div class="col">
+            <!-- Link -->
+            <ul class= "d-flex align-items-center h-100">
+                <li>
+                    <a href="">Characters</a>
+                </li>
+                <li>
+                    <a href="">Comics</a>
+                </li>
+                <li>
+                    <a href="">Movies</a>
+                </li>
+                <li>
+                    <a href="">Games</a>
+                </li>
+                <li>
+                    <a href="">Collectibles</a>
+                </li>
+                <li>
+                    <a href="">Videos</a>
+                </li>
+                <li>
+                    <a href="">Fans</a>
+                </li>
+                <li>
+                    <a href="">News</a>
+                </li>
+            </ul>
+        </div>
+        <!-- FINE COL RIGHT -->
+    </div>
+</div>
+<!-- FINE HEADER -->
