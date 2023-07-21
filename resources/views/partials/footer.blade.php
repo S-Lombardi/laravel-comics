ddc228bfb6c50014f2eed6ac730fac8cf7afff7e
S-Lombardi <silo.lombardi@gmail.com>
2023-07-21 18:03:08 +0200

    Inserito footer nel file footer.blade e inlcuso nella home

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,50 @@
+
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Footer top - BLU -->
+            <div class="col-12 px-5 footer-top">
+
+                <ul class="d-flex align-items-center h-100 justify-content-around">
+
+                    <a href="/" class="text-white text-decoration-none"><li>
+                        <img class="w-25" src="{{Vite::asset('./resources/img/buy-comics-digital-comics.png')}}" alt="">
+                        <span class="ms-3 ">
+                            DIGITAL COMICS
+                        </span>
+                        </li></a>
+                    <a href="/" class="text-white text-decoration-none"><li>
+                        <img class="w-25" src="{{Vite::asset('./resources/img/buy-comics-merchandise.png')}}" alt=""> 
+                        <span class="ms-3 ">
+                            DC MERCHANDISE
+                        </span>
+                    </li></a>
+                    <a href="/" class="text-white text-decoration-none"><li>
+                        <img class="w-25" src="{{Vite::asset('./resources/img/buy-comics-subscriptions.png')}}" alt=""> 
+                        <span class="ms-3 ">
+                            SUBSCRIPTION
+                        </span>
+                    </li></a>
+                    <a href="/" class="text-white text-decoration-none"><li class="d-flex">
+                        <img class="footer-icons w-25" src="{{Vite::asset('./resources/img/buy-comics-shop-locator.png')}}" alt="">
+                        <span class="align-self-center ms-3 ">
+                            COMIC SHOP LOCATOR
+                        </span>
+                        </li></a>
+                    <a href="/" class="text-white text-decoration-none"><li class="text-center d-flex">
+                        <img class="footer-icons w-25" src="{{Vite::asset('./resources/img/buy-dc-power-visa.svg')}}" alt=""> 
+                        <span class="align-self-center ms-3 ">
+                            DC POWER VISA
+                        </span>
+                    </li></a>
+
+                </ul>
+                <!-- FINE Footer top - BLU -->
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        
+    </div>
+</footer>
