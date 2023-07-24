b306200894dab9c80419f8ddfdf353cd803b9a7b
S-Lombardi <silo.lombardi@gmail.com>
2023-07-24 15:33:14 +0200

    Corretto problema sulla visualizzazione del main - chiuso il contenuto dei fumetti con @endsection invece che @section

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,37 +1,34 @@
 
 
-    @extends('layouts.app')  <!-- estendo il contenuto di questa pagina con quello di app.blade -->
+@extends('layouts.app')  <!-- estendo il contenuto di questa pagina con quello di app.blade -->
 
 
-    
+@section ('content')
+<!-- COMICS -->
+<main class="bg-dark p-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex flex-wrap">
 
-    
-    @section ('content')
-    <!-- COMICS -->
-    <main class="bg-dark p-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 d-flex flex-wrap">
-
-                    @foreach($comics as $comic)
-                    <div class="flex-column col-auto col-md-4 col-lg-2 d-flex align-items-center my-3">
-                        <!-- Immagini -->
-                        <img src="{{ $comic['thumb']}}" alt="">
-                        <!-- titoli -->
-                        <div>
-                            <h6 class="text-light text-center text-uppercase ">
-                                {{ $comic['title']}}
-                            </h6>
-                        </div>
+                @foreach($comics as $comic)
+                <div class="flex-column col-auto col-md-4 col-lg-2 d-flex align-items-center my-3">
+                    <!-- Immagini -->
+                    <img src="{{ $comic['thumb']}}" alt="">
+                    <!-- titoli -->
+                    <div>
+                        <h6 class="text-light text-center text-uppercase ">
+                            {{ $comic['title']}}
+                        </h6>
                     </div>
-                    @endforeach()
                 </div>
+                @endforeach()
             </div>
         </div>
+    </div>
 
-    </main>
-    <!-- FINE COMICS -->
-    @section ('content')
+</main>
+<!-- FINE COMICS -->
+@endsection 
 
     
 
