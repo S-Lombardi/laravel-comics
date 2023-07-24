958dbfc599d63b0d82bbc705db0c1a2d9d28176c
S-Lombardi <silo.lombardi@gmail.com>
2023-07-24 17:45:44 +0200

    Aggiunto prezzo e descrizione fumetto

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -8,6 +8,19 @@
         <div class="col">
             {{-- Titolo --}}
             <h1>{{ $comic ['title']}}</h1>
+
+            {{-- Prezzo --}}
+            <div class="bg-success p-3 mb-3">
+                <div>
+                    U.S. Price: {{$comic ['price']}}
+                </div>
+
+            </div>
+
+            {{-- Descrizione --}}
+            <p>
+                {{$comic ['description']}}
+            </p>
         </div>
         <div class="col">
             <h6 class="text-uppercase">Advertisement</h6>
